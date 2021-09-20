5164883f7dc793700a3afb69257279acdcec3e0a
Andrey Popov <andpop@mail.ru>
2021-09-21 00:34:41 +0300

    Add examples in SQL

--- db/02_SQL/examples2.sql
+++ db/02_SQL/examples2.sql
@@ -97,3 +97,79 @@ INSERT INTO `order` (`id`, `summa`, `order_date`, `customer_id`, `vendor_id`) VA
 (3005, 100, '2016-12-16', 2004, 1002),
 (3006, 95.13, '2016-12-15', 2001, 1001);
 
+-- 8. Inner join
+SELECT customer.name, vendor.name, vendor.city FROM vendor, customer WHERE vendor.city=customer.city;
+SELECT customer.name, vendor.name, vendor.city FROM vendor INNER JOIN customer ON vendor.city=customer.city;
+
+-- Inner join with three tables
+SELECT customer.name 'Покупатель', vendor.name 'Продавец', vendor.city, `order`.order_date, `order`.summa
+FROM vendor 
+   INNER JOIN customer 
+      ON vendor.city=customer.city
+   INNER JOIN `order`
+      ON `order`.customer_id=customer.id;
+
+SELECT customer.name 'Покупатель', vendor.name 'Продавец', vendor.city, `order`.order_date, `order`.summa
+FROM `order`
+   INNER JOIN customer
+      ON `order`.customer_id=customer.id
+   INNER JOIN vendor 
+      ON vendor.city=customer.city;
+-- Результат двух запросов один и тот же
+
+SELECT cust_ord.name 'Покупатель', vendor.name 'Продавец', vendor.city, cust_ord.order_date, cust_ord.summa
+FROM vendor 
+   INNER JOIN 
+   (SELECT customer.city, customer.name, `order`.order_date, `order`.summa
+    FROM customer
+       INNER JOIN `order`
+          ON `order`.customer_id=customer.id) cust_ord
+      ON vendor.city=cust_ord.city;
+
+
+-- 7. Groupping
+SELECT vendor_id FROM `order`;
+SELECT vendor_id FROM `order` GROUP BY vendor_id;
+SELECT vendor_id, count(*) FROM `order` GROUP BY vendor_id;
+SELECT vendor_id, count(*) FROM `order` GROUP BY vendor_id HAVING count(*) > 1;
+SELECT vendor_id, order_date, MAX(summa) FROM `order` GROUP BY vendor_id, order_date;
+SELECT vendor_id, order_date, MAX(summa) FROM `order` GROUP BY vendor_id, order_date HAVING MAX(summa)>100;
+
+-- Подзапросы для создания данных
+Определим группы по суммам продаж
+SELECT 'Мало' name, 0 low_sum, 100 high_sum
+UNION
+SELECT 'Средне' name, 100.01 low_sum, 500 high_sum
+UNION
+SELECT 'Много' name, 500.01 low_sum, 100000 high_sum;
+
+Найдем количество и сумму покупок по каждому покупателю
+SELECT customer_id, count(*) num_orders, sum(summa) total
+FROM `order`
+GROUP BY customer_id;
+
+Свяжем с таблицей customer для вывода фамилии покупателя
+SELECT customer.name name, num_orders, total
+FROM customer
+   INNER JOIN 
+        (SELECT customer_id, count(*) num_orders, sum(summa) total
+        FROM `order`
+        GROUP BY customer_id) customer_orders
+      ON customer.id=customer_orders.customer_id;
+
+Свяжем с группами по сумам продаж
+SELECT customer.name name, num_orders, total, sum_groups.name
+FROM customer
+   INNER JOIN 
+        (SELECT customer_id, count(*) num_orders, sum(summa) total
+        FROM `order`
+        GROUP BY customer_id) customer_orders
+      ON customer.id=customer_orders.customer_id
+   INNER JOIN
+        (SELECT 'Мало' name, 0 low_sum, 100 high_sum
+        UNION
+        SELECT 'Средне' name, 100.01 low_sum, 500 high_sum
+        UNION
+        SELECT 'Много' name, 500.01 low_sum, 100000 high_sum) sum_groups
+      ON customer_orders.total BETWEEN sum_groups.low_sum AND sum_groups.high_sum;
+
