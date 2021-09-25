ed130ce5a325f179accda996727757a406f81ea4
Andrey Popov <andpop@mail.ru>
2021-09-25 15:51:33 +0300

    Add examples in SQL

--- db/02_SQL/examples3.sql
+++ db/02_SQL/examples3.sql
@@ -30,7 +30,10 @@ SELECT *, DENSE_RANK() OVER (ORDER BY rating DESC) AS rank FROM customer ORDER B
 SELECT id, name, rating, ROW_NUMBER() OVER (ORDER BY rating DESC) AS row_number,
 RANK() OVER (ORDER BY rating DESC) AS rank, DENSE_RANK() OVER (ORDER BY rating DESC) AS dense_rank FROM customer ORDER BY name;
 
+-- Ранжирование по суммам заказов по годам
+SELECT *, RANK() OVER (PARTITION BY strftime('%Y', order_date) ORDER BY summa DESC) AS rank FROM `order` ORDER BY order_date;
 
+-- Функции агрегирования
 SELECT name, city, rating, AVG(rating) OVER () AS average_rating FROM customer;
 SELECT name, city, rating, rating-AVG(rating) OVER () AS delta_rating FROM customer;
 
@@ -41,6 +44,15 @@ SELECT *, MAX(rating) OVER () FROM customer;
 
 SELECT *, COUNT(*) OVER (PARTITION BY city) AS customers_in_city FROM customer;
 
+-- Рамки окон
+-- Окно данных от начала результирующего набора до текущей строки, включая ее
+SELECT *, sum(summa) OVER (ROWS UNBOUNDED PRECEDING) 'Нарастающий итог' FROM `order`;
+
+
+SELECT id, summa, lag(summa, 1) OVER () 'Предыдущая сумма',
+lead(summa, 1) OVER () 'Следующая сумма' FROM `order`;
+
+--------------------------------------------------------------------
 -- Дублирование данных из таблицы `order` под новой датой
 insert into `order` (summa, order_date, customer_id, vendor_id) select summa, '2021-09-25', customer_id, vendor_id from `order`;
 
