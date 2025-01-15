ef68a198582a413335669db71360087546a98abe
Andrey Popov <andpop@mail.ru>
2025-01-15 17:42:37 +0300

    Add tasks in PHP

--- PHP/04_Language/arrays/destruct.php
+++ PHP/04_Language/arrays/destruct.php
@@ -8,4 +8,8 @@ print_r($arr);
 [$a1, $a2] = $arr;
 echo "a1=$a1 a2=$a2" . PHP_EOL;
 
+// -----------------------------------------------------------------
+$person = ['first' => 'Rasmus', 'last' => 'Lerdorf', 'manager' => true];
 
+['last' => $lastname, 'first' => $firstname] = $person;
+echo "lastname=$lastname firstname=$firstname" . PHP_EOL;
