ac3602027553bb53b75e2dc304e2c64fcf80e11d
Andrey Popov <andpop@mail.ru>
2025-01-18 13:52:27 +0300

    Add examples in 04_Language (PHP)

---
+++ PHP/04_Language/include/include.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Hello from include.php!\n";
+
+echo "a=$a\n";
+
+function f()
+{{
+    echo "Message from f()\n";
+}
+
---
+++ PHP/04_Language/include/main.php
@@ -0,0 +1,14 @@
+<?php
+
+$a = 1;
+
+echo "Hello from main.php\n";
+
+/* f();  // Error, undefined func */
+
+require_once("include.php");
+
+/* f();   // Ok */
+
+/* echo "Bye from main.php\n"; */
+
