faf62d7f09626b7847d56d99afbbfcabb8b13778
Andrey Popov <andpop@mail.ru>
2025-01-25 17:52:56 +0300

    Add examples in 05_OOP (PHP)

--- PHP/04_PHP_Base/functions/named_args.php
+++ PHP/04_PHP_Base/functions/named_args.php
@@ -9,6 +9,11 @@ function createUser(string $username, string $email, int $age = null)
     ];
 }
 
+/* Вызов функции с позиционными аргументами */
+$user = createUser('ivan_ivanov', 'ivan@example.com');
+print_r($user);
+
+/* Вызов функции с именованными аргументами */
 $user = createUser(
     username: 'ivan_ivanov',
     email: 'ivan@example.com'
